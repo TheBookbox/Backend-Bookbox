da708865806152c072a29b84d54a606a99a709fe
Fabrício Silva <fabricio85silva@hotmail.com>
2025-05-24 19:35:50 -0300

    asdkmfd

--- config/db.js
+++ config/db.js
@@ -5,9 +5,10 @@ dotenv.config()
 
 const connectDB = process.env.DB_CONNECT
 
-
 const connectDb = async()=>{
     try {
+        console.log('conectando...');
+        
         const dbConnect = await mongoose.connect(`${connectDB}`)
         console.log('Conectado ao banco');
         return dbConnect
